f505b5d1c1712b440574f7f88d5f1ca11da53bb0
Kyrill <kyrill.spiegl@gmail.com>
2019-12-02 13:26:38 +0900

    initialized MyPanel with Labels and textfields

--- MyPanel.java
+++ MyPanel.java
@@ -1,33 +1,30 @@
-
+import javax.swing.*;
+import java.awt.*;
 /**
  * 여기에 MyPanel 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyPanel
+public class MyPanel extends JPanel
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * MyPanel 클래스의 객체 생성자
-     */
-    public MyPanel()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public MyPanel(){
+        
+        JLabel mlName = new JLabel("이름");
+        JLabel mlDept = new JLabel("학과");
+        JLabel mlAddress = new JLabel("주소");
+        
+        JTextField tfName = new JTextField();
+        JTextField tfDept = new JTextField();
+        JTextField tfAddress = new JTextField();
+        
+        this.add(mlName);
+        this.add(tfName);
+        
+        this.add(mlDept);
+        this.add(tfDept);
+        
+        this.add(mlAddress);
+        this.add(tfAddress);
     }
 }
