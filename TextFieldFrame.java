2ef2d42ba5a10761bb22d7ee1c6e51c288b993f4
jch1999 <elvpfhhrm@gmail.com>
2019-12-02 13:45:11 +0900

    클래스 설명 부분 수정

--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 /**
  * TextFieldFrame에 대한 클래스
  * 
- * @author (2017315056 이토아야카) 
+ * @author (2018243087 정찬호/2017315056 이토아야카/2017605038 스피겔 크릴) 
  * @version (2019.12.02)
  */
 public class TextFieldFrame extends JFrame
